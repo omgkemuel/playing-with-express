0a8137cb3501671439a5378f4357151a76e3a476
Kemuel McDaniel <kemuel.mcdaniel.asurion.com>
2017-01-03 10:35:20 -0600

    convert var to const

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
 
 app.get('/', function(req, res) {
   res.send('Hello World!');
 });
 
-var server = app.listen(1337, function() {
-  var host = server.address().address;
-  var port = server.address().port;
+app.get('/goodbye', function(req, res) {
+  res.send('Goodbye World!');
+});
+
+const server = app.listen(1337, function() {
+  const host = server.address().address;
+  const port = server.address().port;
 
   console.log('Your app is running at http://%s:%s', host,port);
 });
