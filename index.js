0286d8ce6ee0c3b423deb85d8d52a2e201b65c40
Kemuel McDaniel <kemuel.mcdaniel.asurion.com>
2017-01-03 11:56:30 -0600

    added mysql

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
 const express = require('express');
-const app = express();
+const mysql   = require('mysql');
+const app     = express();
 
+//Application Settings
 app.use(express.static('public'));
 app.set('view engine', 'jade');
 
+//Configuation
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'root',
+  database : 'express'
+});
+ 
 app.get('/', function(req, res) {
+  connection.connect();
+ 
+  connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
+    if (err) throw err;
+  
+    console.log('The solution is: ', rows[0].solution);
+  });
+  
+  connection.end();
   res.render('index', {title:'Express Yo!', message: 'Hey Frank!'});
 });
 
